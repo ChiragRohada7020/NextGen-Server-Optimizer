675f6d12b4cb8da0436b995faef9b01545e68408
NextGen-Clouds <nwayzrj@gmail.com>
2025-10-02 13:27:21 +0330

    Create optimizer.sh

---
+++ modules/optimizer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source "$(dirname "$0")/utils.sh"
+
+optimize_system() {
+    log_info "شروع بهینه‌سازی سرور..."
+    sysctl -w net.ipv4.tcp_fin_timeout=10 >/dev/null 2>&1
+    sysctl -w net.ipv4.tcp_tw_reuse=1 >/dev/null 2>&1
+    sysctl -w fs.file-max=2097152 >/dev/null 2>&1
+    log_success "بهینه‌سازی انجام شد!"
+}
