5709753c77629b43dd1aa99a429d2b0c214655b6
Javid <nwayzrj@gmail.com>
2025-10-02 13:54:32 +0330

    Update firewall.sh

--- modules/firewall.sh
+++ modules/firewall.sh
@@ -2,12 +2,12 @@
 source "$(dirname "$0")/utils.sh"
 
 setup_firewall() {
-    log_info "در حال تنظیم فایروال..."
+    log_info "Waiting For Firewall..."
     ufw disable >/dev/null 2>&1
     ufw reset >/dev/null 2>&1
     ufw allow ssh >/dev/null 2>&1
     ufw default deny incoming >/dev/null 2>&1
     ufw default allow outgoing >/dev/null 2>&1
     yes | ufw enable >/dev/null 2>&1
-    log_success "فایروال با موفقیت تنظیم شد!"
+    log_success "Firewall Activated!"
 }
