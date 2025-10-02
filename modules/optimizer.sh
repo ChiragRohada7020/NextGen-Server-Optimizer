2eb2728f44529300f67475f7dc185194275269cb
Javid <nwayzrj@gmail.com>
2025-10-02 13:56:20 +0330

    Update optimizer.sh

--- modules/optimizer.sh
+++ modules/optimizer.sh
@@ -2,9 +2,9 @@
 source "$(dirname "$0")/utils.sh"
 
 optimize_system() {
-    log_info "شروع بهینه‌سازی سرور..."
+    log_info "Installing Optimizer..."
     sysctl -w net.ipv4.tcp_fin_timeout=10 >/dev/null 2>&1
     sysctl -w net.ipv4.tcp_tw_reuse=1 >/dev/null 2>&1
     sysctl -w fs.file-max=2097152 >/dev/null 2>&1
-    log_success "بهینه‌سازی انجام شد!"
+    log_success "Optimizer Succefully!"
 }
