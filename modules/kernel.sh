eedb1abedfc1f206159bed9946966bdf9caa5227
Javid <nwayzrj@gmail.com>
2025-10-02 13:55:21 +0330

    Update kernel.sh

--- modules/kernel.sh
+++ modules/kernel.sh
@@ -2,8 +2,8 @@
 source "$(dirname "$0")/utils.sh"
 
 install_xanmod() {
-    log_info "نصب Xanmod Kernel..."
+    log_info "Installing Xanmod Kernel..."
     apt update -y >/dev/null 2>&1
     apt install -y linux-xanmod >/dev/null 2>&1
-    log_success "Xanmod Kernel نصب شد!"
+    log_success "Xanmod Kernel Installed !"
 }
